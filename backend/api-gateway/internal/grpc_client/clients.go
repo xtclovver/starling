9478d93e1bfb3ecb0733f5355aee4b536522fe27
xtclovver <notcvnt@gmail.com>
2026-03-30 01:46:05 +0300

    dns round-robin for gRPC services

--- backend/api-gateway/internal/grpc_client/clients.go
+++ backend/api-gateway/internal/grpc_client/clients.go
@@ -21,9 +21,16 @@ type Clients struct {
 	conns []*grpc.ClientConn
 }
 
+const roundRobinServiceConfig = `{"loadBalancingPolicy":"round_robin"}`
+
+func dnsAddr(addr string) string {
+	return "dns:///" + addr
+}
+
 func New(userAddr, postAddr, commentAddr, mediaAddr string) (*Clients, error) {
 	opts := []grpc.DialOption{
 		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithDefaultServiceConfig(roundRobinServiceConfig),
 		grpc.WithKeepaliveParams(keepalive.ClientParameters{
 			Time:                30 * time.Second,
 			Timeout:             10 * time.Second,
@@ -31,25 +38,25 @@ func New(userAddr, postAddr, commentAddr, mediaAddr string) (*Clients, error) {
 		}),
 	}
 
-	userConn, err := grpc.NewClient(userAddr, opts...)
+	userConn, err := grpc.NewClient(dnsAddr(userAddr), opts...)
 	if err != nil {
 		return nil, err
 	}
 
-	postConn, err := grpc.NewClient(postAddr, opts...)
+	postConn, err := grpc.NewClient(dnsAddr(postAddr), opts...)
 	if err != nil {
 		userConn.Close()
 		return nil, err
 	}
 
-	commentConn, err := grpc.NewClient(commentAddr, opts...)
+	commentConn, err := grpc.NewClient(dnsAddr(commentAddr), opts...)
 	if err != nil {
 		userConn.Close()
 		postConn.Close()
 		return nil, err
 	}
 
-	mediaConn, err := grpc.NewClient(mediaAddr, opts...)
+	mediaConn, err := grpc.NewClient(dnsAddr(mediaAddr), opts...)
 	if err != nil {
 		userConn.Close()
 		postConn.Close()
